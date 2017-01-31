a9f21da5bced1735e9823c6ab5b6ffc07c20c124
Neeraj <neeraj.kumar@acronis.com>
2017-01-31 16:45:46 +0800

    golang examples

---
+++ mix_var_decl.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a, b, c = 1, 3.4, "abc"
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+}
---
+++ type_inference.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var x float32 = 34.56
+
+  y := 43
+
+  fmt.Println(x)
+  fmt.Println(y)
+  fmt.Printf("x is %T\n",x);
+  fmt.Printf("y is %T\n",y);
+}
