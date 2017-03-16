65b672b18e91f4055bb0b1f18f58e0b773e98d63
Neeraj <nir.neeraj@gmail.com>
2017-03-16 14:57:07 +0800

    readline

---
+++ readline.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s := scanner.Text()
+		fmt.Println(s)
+	}
+
+	if err := scanner.Err(); err != nil {
+		os.Exit(1)
+	}
+}
