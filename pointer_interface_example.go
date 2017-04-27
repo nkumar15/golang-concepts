efb1e9cf771acaa10754ee4f3af23dbc938028ed
Neeraj <nir.neeraj@gmail.com>
2017-04-27 14:07:25 +0800

    pointer_interface_example

--- pointer_interface_example.go
+++ pointer_interface_example.go
@@ -1,14 +1,37 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"os"
-)
+import "fmt"
+
+type CacheItem interface {
+	GetId() string
+}
+
+//Application
+type Application struct {
+	id string
+}
+
+func (a *Application) GetId() string {
+	return a.id
+}
+
+type User struct {
+	name string
+}
+
+func (a User) GetId() string {
+	return a.name
+}
 
 func main() {
-	var w io.Writer
-	w = os.Stdout
 
-	fmt.Fprintf(w, "Hello\n")
+	var var1 CacheItem = &Application{"Application"}
+	//var var2 CacheItem = Application{"Application"}
+	app := Application{"Application"}
+	app.GetId()
+	fmt.Println(var1.GetId()) //, var2.GetId())
+
+	// var var4 CacheItem = &User{"neeraj"}
+	// var var3 CacheItem = User{"neeraj"}
+	// fmt.Println(var3.GetId(), var4.GetId())
 }
